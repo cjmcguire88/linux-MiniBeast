ffcec30b5c7f69cd38d5106ec86918d8cd7aa20a
cjmcguire88 <cjmcguire88@gmail.com>
2021-02-09 14:49:01 -0500

    2/9/21

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#2 SMP PREEMPT Mon Feb 8 22:25:04 EST 2021"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "MiniBeast"
+#define LINUX_COMPILER "gcc (GCC) 10.2.0, GNU ld (GNU Binutils) 2.36"
